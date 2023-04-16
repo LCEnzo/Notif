4e100ee863a6e2736c42e02599732906762cac01
Luka Colić <lcenzo@protonmail.ch>
2023-04-16 19:05:25 +0200

    Fix project structure and issues due to renaming

--- backend/notif/asgi.py
+++ backend/notif/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for notif_back project.
+ASGI config for notif project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notif_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notif.settings')
 
 application = get_asgi_application()
