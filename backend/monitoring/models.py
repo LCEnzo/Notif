64e11c5469992dc705639ef9a04421fadaff549d
Luka Colić <luka.colic@qcerris.com>
2023-04-18 18:53:12 +0200

    Updated models for User and links

--- backend/monitoring/models.py
+++ backend/monitoring/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Link(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
