3c89939cfe65e04a66b8c4abd43cc247eea7a3f0
Luka Colić <lcenzo@protonmail.ch>
2023-04-18 18:53:12 +0200

    Updated models for User and links

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, Group, Permission
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, username: str, password: str, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, username: str, password: str, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, username, password, **extra_fields)        
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(
+        Group, 
+        blank=True,
+        related_name='users', 
+        related_query_name='users',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        blank=True,
+        related_name='users', 
+        related_query_name='users',
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'email', 'password']
