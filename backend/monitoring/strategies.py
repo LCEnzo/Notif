ce621720aa6897f22be746a83003ad5fe7653e93
Luka Colić <lcenzo@protonmail.ch>
2023-05-15 13:56:24 +0200

    Finish preliminary GeneralSelectorStrategy

--- backend/monitoring/strategies.py
+++ backend/monitoring/strategies.py
@@ -1,6 +1,24 @@
 # This file contains scraping strategies/callables
 # This whole file is TODO
 
+from typing import TypeAlias, Any
+from abc import ABC, abstractmethod
+from urllib.parse import urlsplit
+
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet, Tag
+
+from rest_framework import status
+
+# Types
+# TODO: Think of moving them to a centreal location so that the whole app can use them
+URL: TypeAlias = str
+NotifData: TypeAlias = list[tuple[str, str, str]]
+DataDict: TypeAlias = None | dict[str, Any]
+NotifDataOrError: TypeAlias = str | NotifData
+
+
 registry = {}
 
 def register(cls):
@@ -11,20 +29,95 @@ def register(cls):
 # Used for choices for the Strategy model
 STRATEGY_CHOICES = [(name, name) for name in registry.keys()]
 
+def _fetch_url_content(url: URL) -> str | None:
+    response = requests.get(url)
+    if response.status_code == status.HTTP_200_OK:
+        return response.text
+    else:
+        return None
 
-class BaseStrategy():
-    def validateUrl(self, url):
+def _get_content_with_css_selector(html_content: str, css_selector: str) -> ResultSet[Tag]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    elements = soup.select(css_selector)
+    return elements
+
+
+class BaseStrategy(ABC):
+    @abstractmethod
+    def can_scrape_url(self, url: URL) -> bool:
         """
-        This function exists to check if the strategy CAN scrape the URL. Whether that be a hardcoded list of sites, or whatever.
+        This function exists to check if the strategy CAN scrape the URL. 
+        Whether that be a hardcoded list of sites, or whatever.
         """
         pass
 
-    def scrape(self, url, data):
+    @abstractmethod
+    def scrape(self, url: URL, config_data: dict, comparison_data: dict, 
+               *args, **kwargs) -> tuple[NotifDataOrError, DataDict]:
         """
         This function is the basic functionality. It takes in the URL to be scraped, as well as JSON data.
-        It should return whether there was new stuff found, and if so, what it is. Return an Optional? <- TODO
+        It should return whether there was new stuff found, and if so, what it is. 
+        Returns None in case nothing was found, str for errors, and the list of tuples containing a 
+        title, descrpition, and link in case of new content. It also returns new data if there is any.
         """
         pass
+
+    def __call__(self, url: URL, config_data: dict, comparison_data: dict, 
+                 *args, **kwargs) -> tuple[NotifDataOrError, DataDict]:
+        return self.scrape(url, config_data, comparison_data, args, kwargs)
+
+
+@register
+class GeneralSelectorStrategy(BaseStrategy):
+    """
+    Uses a CSS selector to check just some element on a page, instead of the whole thing.
+    """
+    def can_scrape_url(self, url: URL) -> bool:
+        return True
+
+    def scrape(self, url: URL, config_data: dict[str, Any], comparison_data: dict[str, list[int]], 
+               *args, **kwargs) -> tuple[NotifDataOrError, DataDict]:
+        selectors: list[str] = config_data['selectors'] 
+        old_data: dict[str, list[int]] = { 
+            selector: 
+                comparison_data.get(str(selector), []) 
+            for selector in selectors 
+        }
+        updates: NotifData = []
+        new_data: dict[str, list[int]] | None = None
+
+        html_content = _fetch_url_content(url)
+        if html_content is None:
+            return "Empty html_content", None
+
+        new_data = {
+            selector: 
+                list(
+                    [hash(ret) for ret in _get_content_with_css_selector(html_content, selector)]
+                ) 
+            for selector in selectors
+        }
+
+        for selector in selectors:
+            tags = new_data.get(f'{selector}', [])
+            old_tags = old_data.get(f'{selector}', [])
+            update = False
+
+            if len(tags) != len(old_tags):
+                update = True
+            else:
+                update = tags != old_tags
+            
+            if update:
+                split = urlsplit(url)
+                site_name = split.netloc.split('.')[0]
+                updates += [(f'{site_name} update', f'selector: {selector}', url)]
+
+        # No need to save new info to the db if the new info is the same as the old info
+        if len(updates) == 0:
+            new_data = None
+
+        return updates, new_data
 
 
 class QQLogInMixin():
@@ -38,24 +131,16 @@ class QQLogInMixin():
 
 
 @register
-class XenoforoThreadmarksStrategy(BaseStrategy):
+class SpaceBattlesThreadmarksStrategy(BaseStrategy):
     """
-    Check Xenoforo forum threadmarks. Should optionally have log in functionality.
-    """
-    pass
-
-
-@register
-class XenoforoAlertsStrategy(BaseStrategy, QQLogInMixin):
-    """
-    Check QQ threadmarks. Needs to be able to login.
+    Check SpaceBattles threadmarks. Configuration should specify which threadmark tabs should be checked.
     """
     pass
 
 
 @register
-class GeneralSelectorStrategy(BaseStrategy):
+class QQAlertsStrategy(BaseStrategy, QQLogInMixin):
     """
-    Uses a CSS selector to check just some element on a page, instead of the whole thing.
+    Check QQ alerts. Needs to be able to login.
     """
     pass
--- backend/monitoring/tests.py
+++ backend/monitoring/tests.py
@@ -1,3 +1,27 @@
+import logging
+
 from django.test import TestCase
 
-# Create your tests here.
+from monitoring.strategies import _fetch_url_content, _get_content_with_css_selector, GeneralSelectorStrategy
+
+logger = logging.getLogger(__name__)
+
+
+class TestSelectorStrat(TestCase):
+    def test_selector_strat(self):
+        strat = GeneralSelectorStrategy()
+
+        url = "https://kemono.party/patreon/user/50187986" 
+        config_data = { "selectors": ["article.post-card"] }
+        old_data = {}
+        
+        notif_data, new_data = strat(url, config_data, old_data)
+        
+        logger.debug(
+            f"selector strat on kemono: \t" + 
+            f"{notif_data} \n--------------------------\n" + 
+            f"{new_data}\n--------------------------\n"
+        )
+
+        assert type(notif_data) != str
+        assert new_data is not None
