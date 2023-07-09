868b5f55e8dd843b1f0d1128ef1c7b59ef853e79
Luka Colić <lcenzo@protonmail.ch>
2023-07-09 19:29:34 +0200

    Fix permissions bugs

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -14,7 +14,7 @@ from commons.permissions import IsRequestingThemselves, ReadOnly
 
 
 class UserViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated, ReadOnly | IsAdminUser | IsRequestingThemselves]
+    permission_classes = [IsAuthenticated, (ReadOnly | IsRequestingThemselves | IsAdminUser)]
     queryset = User.objects.all()
     
     def get_serializer_class(self) -> type[BaseSerializer]:
@@ -31,7 +31,7 @@ class UserViewSet(ModelViewSet):
 
     def get_permissions(self):
         # Account creation, ie. registration, needs to work for visitors without an account
-        if self.request.method == "POST" and self.kwargs.get('pk', None) is not None:
-            return []
+        if self.request.method == "POST": 
+            return [IsAuthenticated()]
 
         return super().get_permissions()
