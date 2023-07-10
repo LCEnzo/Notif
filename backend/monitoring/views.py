145fdc8f4827d1b478e2cbb1ce5f3937c92e04e8
Luka Colić <lcenzo@protonmail.ch>
2023-07-10 23:18:06 +0200

    Add initial implementations of Link and Strategy CRUD endpoints, move some test logic to common, fix a bug with logging

--- backend/monitoring/views.py
+++ backend/monitoring/views.py
@@ -1,2 +1,19 @@
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from commons.permissions import ReadOnly
+from monitoring.models import Link, Strategy
+from monitoring.serializers import LinkSerializer, StrategySerializer
+
+
+class LinkViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated, ReadOnly | IsAdminUser]
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+
+
+class StrategyViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated, ReadOnly | IsAdminUser]
+    queryset = Strategy.objects.all()
+    serializer_class = StrategySerializer
+
