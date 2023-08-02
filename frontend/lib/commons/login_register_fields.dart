aa3e9df0b0761bf5dd9f2618fdd00cbeca4310c8
Luka Colić <luka.colic@qcerris.com>
2023-08-02 20:35:46 +0200

    Refactor LogInPage, add RegisterPage, move some widgets to commons.

---
+++ frontend/lib/commons/login_register_fields.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Logo extends StatelessWidget {
+  final String title;
+  const Logo({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FlutterLogo(size: isSmallScreen ? 100 : 200),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: isSmallScreen
+                ? Theme.of(context).textTheme.headlineSmall
+                : Theme.of(context)
+                    .textTheme
+                    .headlineMedium
+                    ?.copyWith(color: Colors.black),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class UsernameTextField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+  final TextEditingController textController;
+
+  const UsernameTextField(
+      {Key? key,
+      required this.labelText,
+      required this.hintText,
+      required this.textController})
+      : super(key: key);
+
+  @override
+  _UsernameTextField createState() =>
+      _UsernameTextField(textController: textController);
+}
+
+class _UsernameTextField extends State<UsernameTextField> {
+  final TextEditingController textController;
+
+  _UsernameTextField({required this.textController});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textController,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        prefixIcon: const Icon(Icons.account_box_outlined),
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
+
+class EmailTextField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+  final String? Function(String?)? validator;
+  final TextEditingController textController;
+
+  const EmailTextField(
+      {Key? key,
+      required this.labelText,
+      required this.hintText,
+      this.validator,
+      required this.textController})
+      : super(key: key);
+
+  @override
+  _EmailTextFieldState createState() =>
+      _EmailTextFieldState(textController: textController);
+}
+
+class _EmailTextFieldState extends State<EmailTextField> {
+  late String? Function(String?) validator;
+  final TextEditingController textController;
+
+  _EmailTextFieldState({required this.textController});
+
+  @override
+  void initState() {
+    super.initState();
+    validator = widget.validator ?? defaultValidator;
+  }
+
+  String? defaultValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email address';
+    } else if (!RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      controller: textController,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        prefixIcon: const Icon(Icons.email_outlined),
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
+
+class PasswordTextField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+  final String? Function(String?)? validator;
+  final TextEditingController textController;
+
+  const PasswordTextField(
+      {Key? key,
+      required this.labelText,
+      required this.hintText,
+      this.validator,
+      required this.textController})
+      : super(key: key);
+
+  @override
+  _PasswordTextFieldState createState() =>
+      _PasswordTextFieldState(textController: textController);
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool _isPasswordVisible = false;
+  late String? Function(String?) validator;
+  final TextEditingController textController;
+
+  _PasswordTextFieldState({required this.textController});
+
+  @override
+  void initState() {
+    super.initState();
+    validator = widget.validator ?? EntropyValidator().validate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textController,
+      validator: validator,
+      obscureText: !_isPasswordVisible,
+      decoration: InputDecoration(
+          labelText: widget.labelText,
+          hintText: widget.hintText,
+          prefixIcon: const Icon(Icons.lock_outline_rounded),
+          border: const OutlineInputBorder(),
+          suffixIcon: IconButton(
+            icon: Icon(
+                _isPasswordVisible ? Icons.visibility_off : Icons.visibility),
+            onPressed: () {
+              setState(() {
+                _isPasswordVisible = !_isPasswordVisible;
+              });
+            },
+          )),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String buttonText;
+  final Function onPressed;
+  final Color? buttonColor;
+
+  const CustomButton(
+      {Key? key,
+      required this.buttonText,
+      required this.onPressed,
+      this.buttonColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor ?? Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        ),
+        onPressed: onPressed as void Function(),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            buttonText,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EntropyValidator {
+  static const double timeInSeconds =
+      100 * 365 * 24 * 60 * 60; // 100 years in seconds
+  static const double attemptsPerSecond =
+      1e9; // Estimated attempts per second for a consumer CPU
+  static final double minEntropy = log(timeInSeconds * attemptsPerSecond) / ln2;
+
+  String? validate(String? password) {
+    if (password == null || password.isEmpty) {
+      return "Password cannot be empty";
+    }
+
+    double passwordEntropy = calculatePasswordEntropy(password);
+    if (passwordEntropy < minEntropy) {
+      return getHelpText(passwordEntropy: passwordEntropy);
+    }
+
+    return null;
+  }
+
+  String getHelpText({double? passwordEntropy}) {
+    String passwordString = passwordEntropy == null
+        ? ""
+        : "Your password has ${passwordEntropy.toStringAsFixed(2)} bits of entropy. ";
+
+    // ignore: prefer_interpolation_to_compose_strings
+    return "Use more and different characters. \"S0m3 password!\"";
+  }
+
+  double calculatePasswordEntropy(String password) {
+    bool hasDigit = password.contains(RegExp(r'\d'));
+    bool hasLower = password.contains(RegExp(r'[a-z]'));
+    bool hasUpper = password.contains(RegExp(r'[A-Z]'));
+    bool hasPunct = password.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]'));
+    bool hasOther = password.contains(RegExp(r'[^\w\s]'));
+
+    List<bool> categories = [hasDigit, hasLower, hasUpper, hasPunct, hasOther];
+    List<int> lengths = [10, 26, 26, 32, 40];
+    int charSet = 0;
+
+    for (var i = 0; i < categories.length; i++) {
+      if (categories[i]) {
+        charSet += lengths[i];
+      }
+    }
+
+    double passwordEntropy = log(charSet) / ln2 * password.length;
+
+    return passwordEntropy;
+  }
+}
